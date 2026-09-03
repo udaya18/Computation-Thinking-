d452c8f2f019b1b101aa2f62c7924b991aba6286
udaya18 <142873298+udaya18@users.noreply.github.com>
2026-09-03 12:14:15 +0530

    Add files via upload

---
+++ mergesort.py
@@ -0,0 +1,29 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    merged = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+
+    return merged
+
+
+numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
+
+sorted_numbers = merge_sort(numbers)
+print("Sorted list:", sorted_numbers)
